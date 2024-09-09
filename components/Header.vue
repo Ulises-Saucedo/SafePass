93789a0a94db33aa686bce679caa3989ed4eb822
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-09 20:43:32 -0300

    Header component added

--- components/Header.vue
+++ components/Header.vue
@@ -1,4 +1,19 @@
+<script setup lang="ts">
+const emits = defineEmits(["toggle-sidebar"]);
+
+const toggleSidebar = () => {
+  emits("toggle-sidebar");
+};
+</script>
+
 <template>
-  <h2>Holaas</h2>
-  <Icon name="tabler:menu-2" class="text-black/75 text-2xl" />
+  <header class="sticky top-0 w-full border-b bg-white">
+    <nav class="flex items-center px-6 py-4">
+      <Icon
+        name="tabler:menu-2"
+        class="cursor-pointer text-black/75 text-2xl"
+        @click="toggleSidebar"
+      />
+    </nav>
+  </header>
 </template>
