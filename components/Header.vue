5be8ea72c84e093ca7e946893bf5dd1e1e58f88c
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-08 23:18:19 -0300

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Holaas</h2>
+  <Icon name="tabler:menu-2" class="text-black/75 text-2xl" />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/icon",
+    "@nuxtjs/supabase",
+  ],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  },
+});
