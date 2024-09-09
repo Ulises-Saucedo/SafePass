79f8dcc70978ed87625e50a0ba705e161e43f0bb
Ulises <ulisesaucedo.se04@gmail.com>
2024-09-09 20:43:51 -0300

    default layout added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,47 @@
+<script setup lang="ts">
+const showSidebar: Ref<boolean> = ref(false);
+const toggleSidebar = () => (showSidebar.value = !showSidebar.value);
+const links: Link[] = [
+  {
+    name: "Inicio",
+    to: "/",
+    icon: "tabler:home",
+  },
+  {
+    name: "Mis contraseñas",
+    to: "/my-passwords",
+    icon: "tabler:lock-password",
+  },
+  {
+    name: "Iniciar sesión",
+    to: "/auth/signin",
+    icon: "tabler:user-square",
+  },
+];
+</script>
+
 <template>
-  <Header />
+  <Header @toggle-sidebar="toggleSidebar" />
+  <Transition name="fade">
+    <SideBar
+      v-show="showSidebar"
+      :links="links"
+      @toggle-sidebar="toggleSidebar"
+    />
+  </Transition>
+
   <slot />
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+</style>
